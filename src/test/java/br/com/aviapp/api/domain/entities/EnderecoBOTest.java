e81145d783e59fba5cce3e91d79ea76f5505e4e8
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-12-06 21:21:02 -0300

    test: should instanciate with correct values

---
+++ src/test/java/br/com/aviapp/api/domain/entities/EnderecoBOTest.java
@@ -0,0 +1,20 @@
+package br.com.aviapp.api.domain.entities;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class EnderecoBOTest {
+  @Test
+  void shouldInstanciateWithCorrectValues() {
+    EnderecoBO sut = new EnderecoBO(null, "Rua Xavantes", "85806020", "Santa Cruz", "Cascavel", "Paraná");
+
+    assertAll("Match values",
+        () -> assertEquals("Rua Xavantes", sut.getLogradouro()),
+        () -> assertEquals("85806020", sut.getCep()),
+        () -> assertEquals("Santa Cruz", sut.getBairro()),
+        () -> assertEquals("Cascavel", sut.getCidade()),
+        () -> assertEquals("Paraná", sut.getEstado()));
+  }
+}
