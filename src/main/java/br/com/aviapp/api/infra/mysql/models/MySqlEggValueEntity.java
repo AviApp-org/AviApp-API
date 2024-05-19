55592a9cf02939e3c8c1ac28f05c5b5d45243e96
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-05-18 21:40:57 -0300

    feat: add mysql egg value entity

---
+++ src/main/java/br/com/aviapp/api/infra/mysql/models/MySqlEggValueEntity.java
@@ -0,0 +1,30 @@
+package br.com.aviapp.api.infra.mysql.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "egg_value")
+public class MySqlEggValueEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "egg_id")
+  private Integer eggId;
+
+  private LocalDateTime timestamp;
+
+  private BigDecimal value;
+}
