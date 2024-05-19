d4f5cd8a9eab845e5b3ffd24e52c6cca3e9447a5
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-05-19 13:26:58 -0300

    feat: add mysql batch entity

---
+++ src/main/java/br/com/aviapp/api/infra/mysql/models/MySqlBatchEntity.java
@@ -0,0 +1,23 @@
+package br.com.aviapp.api.infra.mysql.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "batch")
+public class MySqlBatchEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "farm_id")
+  private MySqlFarmEntity farmId;
+}
