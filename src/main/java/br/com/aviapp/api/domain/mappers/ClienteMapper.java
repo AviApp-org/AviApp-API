4ce6adaf6d1deb9d877c038f7d3e9d82d202b25b
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-07-03 21:55:24 -0300

    feat: add mapping from client entity to dto

--- src/main/java/br/com/aviapp/api/domain/mappers/ClienteMapper.java
+++ src/main/java/br/com/aviapp/api/domain/mappers/ClienteMapper.java
@@ -2,10 +2,11 @@ package br.com.aviapp.api.domain.mappers;
 
 import br.com.aviapp.api.domain.dto.ClientDTO;
 import br.com.aviapp.api.domain.entities.ClienteBO;
+import br.com.aviapp.api.infra.mysql.models.MySqlClientEntity;
 
 public class ClienteMapper {
   public static ClienteBO toBO(ClientDTO dto) {
-    ClienteBO bo = new ClienteBO(dto.getId(), dto.getName(), dto.getEmail(), dto.getCpf(), dto.getTelefone());
+    ClienteBO bo = new ClienteBO(dto.getId(), dto.getName(), dto.getEmail(), dto.getCpf(), dto.getPhone());
     bo.setStatus(dto.getStatus());
     return bo;
   }
@@ -18,7 +19,20 @@ public class ClienteMapper {
     dto.setCpf(bo.getCpf());
     dto.setName(bo.getName());
     dto.setStatus(bo.getStatus());
-    dto.setTelefone(bo.getPhone());
+    dto.setPhone(bo.getPhone());
+
+    return dto;
+  }
+
+  public static ClientDTO toDTO(MySqlClientEntity entity) {
+    ClientDTO dto = new ClientDTO();
+
+    dto.setId(entity.getId());
+    dto.setEmail(entity.getEmail());
+    dto.setCpf(entity.getCpf());
+    dto.setName(entity.getName());
+    dto.setStatus(entity.getStatus());
+    dto.setPhone(entity.getPhone());
 
     return dto;
   }
