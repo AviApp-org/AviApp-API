262558bbdd646819a10d255a6d878be548180843
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-07-10 18:15:18 -0300

    refactor: refactor test cases

--- src/test/java/br/com/aviapp/api/domain/entities/ClienteBOTest.java
+++ src/test/java/br/com/aviapp/api/domain/entities/ClienteBOTest.java
@@ -3,6 +3,8 @@ package br.com.aviapp.api.domain.entities;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 import br.com.aviapp.api.domain.enums.ClientStatusEnum;
@@ -10,9 +12,15 @@ import br.com.aviapp.api.domain.errors.InvalidParamError;
 
 public class ClienteBOTest {
 
+  private ClientBO makeClientBO() {
+    ClientBO bo = new ClientBO(1L, "Test", "teste@teste.com", "00000000000", "000000000", LocalDate.now());
+
+    return bo;
+  }
+
   @Test
   void shouldInactivateAClient() {
-    ClientBO bo = new ClientBO(1l, "Test", "teste@teste.com", "00000000000", "000000000");
+    ClientBO bo = this.makeClientBO();
     bo.deactivate();
 
     assertEquals(ClientStatusEnum.INACTIVE, bo.getStatus());
@@ -20,7 +28,7 @@ public class ClienteBOTest {
 
   @Test
   void shouldActivateAClient() {
-    ClientBO bo = new ClientBO(1l, "Test", "teste@teste.com", "00000000000", "000000000");
+    ClientBO bo = this.makeClientBO();
     bo.activate();
 
     assertEquals(ClientStatusEnum.ACTIVE, bo.getStatus());
@@ -29,35 +37,35 @@ public class ClienteBOTest {
   @Test
   void shouldThrowIfEmailIsNull() {
     assertThrows(InvalidParamError.class, () -> {
-      new ClientBO(1l, "Test", null, "00000000000", "000000000");
+      new ClientBO(1l, "Test", null, "00000000000", "000000000", LocalDate.now());
     });
   }
 
   @Test
-  void shouldThrowIfCnpjIsNull() {
+  void shouldThrowIfCpfIsNull() {
     assertThrows(InvalidParamError.class, () -> {
-      new ClientBO(1l, "Test", "test@email.com", null, "000000000");
+      new ClientBO(1l, "Test", "test@email.com", null, "000000000", LocalDate.now());
     });
   }
 
   @Test
   void shouldThrowIfNameIsNull() {
     assertThrows(InvalidParamError.class, () -> {
-      new ClientBO(1l, null, "test@email.com", "00000000000", "000000000");
+      new ClientBO(1l, null, "test@email.com", "00000000000", "000000000", LocalDate.now());
     });
   }
 
   @Test
   void shouldThrowIfTelefoneIsNull() {
     assertThrows(InvalidParamError.class, () -> {
-      new ClientBO(1l, "Test", "test@email.com", "00000000000", null);
+      new ClientBO(1l, "Test", "test@email.com", "00000000000", null, LocalDate.now());
     });
   }
 
   @Test
   void shouldThrowIfParamIsBlank() {
     assertThrows(InvalidParamError.class, () -> {
-      new ClientBO(1l, "", "test@email.com", "00000000000", "000000000");
+      new ClientBO(1l, "", "test@email.com", "00000000000", "000000000", LocalDate.now());
     });
   }
 }
