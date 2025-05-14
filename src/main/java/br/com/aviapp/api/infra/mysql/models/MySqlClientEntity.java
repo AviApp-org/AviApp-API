8d613987d2d90b7413c9c490b9de7e549519f01a
Gabriel Mazzo <gabrielmazzo2014@gmail.com>
2025-05-13 21:38:22 -0300

    refac: adding validation for duplicated cnpj

--- src/main/java/br/com/aviapp/api/infra/mysql/models/MySqlClientEntity.java
+++ src/main/java/br/com/aviapp/api/infra/mysql/models/MySqlClientEntity.java
@@ -1,20 +1,13 @@
 package br.com.aviapp.api.infra.mysql.models;
 
 import br.com.aviapp.api.infra.mysql.enums.ClientStatusType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "client")
+@Table(name = "client", uniqueConstraints={@UniqueConstraint(columnNames={"cnpj"})})
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -26,7 +19,7 @@ public class MySqlClientEntity {
   @Column(nullable = false)
   private String name;
 
-  @Column(nullable = false, length = 14)
+  @Column(nullable = false, length = 14 )
   private String cnpj;
 
   @Column(nullable = false)
