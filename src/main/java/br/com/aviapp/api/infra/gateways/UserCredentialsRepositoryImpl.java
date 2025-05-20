ec65079910eb02e6e14906069df1439bf82b5ed1
Gabriel Mazzo <gabrielmazzo2014@gmail.com>
2025-05-20 20:46:08 -0300

    feat:remo secu

--- src/main/java/br/com/aviapp/api/infra/gateways/UserCredentialsRepositoryImpl.java
+++ src/main/java/br/com/aviapp/api/infra/gateways/UserCredentialsRepositoryImpl.java
@@ -1,9 +1,6 @@
 package br.com.aviapp.api.infra.gateways;
 
 import br.com.aviapp.api.infra.mysql.repository.UserCredentialsRepositoryJPA;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
 @Repository
