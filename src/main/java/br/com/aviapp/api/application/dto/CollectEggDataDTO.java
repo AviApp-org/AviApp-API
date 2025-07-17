2a6d955f46ba40d4fd6d3f2c7e15da6b93318878
gabrielmqc <gabrielmazzo2014@gmail.com>
2025-07-17 09:48:10 -0300

    feat: small changes

--- src/main/java/br/com/aviapp/api/application/dto/CollectEggDataDTO.java
+++ src/main/java/br/com/aviapp/api/application/dto/CollectEggDataDTO.java
@@ -5,6 +5,6 @@ import br.com.aviapp.api.domain.entities.EggDetailBO;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record CollectEggDataDTO(Long id, Long aviaryId, List<EggDetailDTO> eggDetail, LocalDateTime collectionDate) {
+public record CollectEggDataDTO(Long id, Long aviaryId, List<EggDetailDTO> eggDetails, LocalDateTime collectionDate) {
 
 }
